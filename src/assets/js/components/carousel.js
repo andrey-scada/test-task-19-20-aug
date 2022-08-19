49ce4c2d112b3a12e474aa51e6b1c6826e37f3a8
Andrey Buchnev <andrey@scada.lv>
2022-08-19 16:10:25 +0300

    add header

---
+++ src/assets/js/components/carousel.js
@@ -0,0 +1,20 @@
+import Flickity from 'flickity';
+
+export default function() {
+  const headerEl = document.querySelector('[data-carousel]');
+
+  const headerOptions = {
+    cellAlign: 'left',
+    contain: true,
+    wrapAround: true,
+    autoPlay: true,
+    autoPlay: 7000,
+    pauseAutoPlayOnHover: false,
+    prevNextButtons: false,
+    draggable: false,
+    selectedAttraction: 0.01,
+    friction: 0.15
+  }
+
+  const headerCarousel = new Flickity(headerEl, headerOptions);
+}
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,11 @@
 import $ from 'jquery';
 
+import carousel from './components/carousel';
 import nav from './components/nav';
 import time from './components/time';
 
 $(document).ready(() => {
+  carousel();
   nav();
   time();
 });
